31b6e6be4ffa6080f347e29ff7243474cf6e4ac0
Stephen Carter <stephen.carter@mail.mcgill.ca>
2015-09-15 11:58:53 -0400

    final

--- q3.c
+++ q3.c
@@ -3,56 +3,42 @@
 #include <stdlib.h>
 #include <time.h>
 
-
 void *init_rand(void *args);
 
 int *result;
-static int num = 0;
+int num = 0;
 pthread_mutex_t *check;
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
   srand(time(NULL));
 
   int N = atoi(argv[1]);
   int s;
-
   pthread_t thread[N];
 
   result = malloc(sizeof(int)*N);
-  fprintf(stderr, "size: %d\n", sizeof(result));
-  fprintf(stderr, "N = %d\n", N);
-
   check = malloc(sizeof(pthread_mutex_t));
   pthread_mutex_init(check, NULL);
 
   int i;
-  for (i = 0; i < N; i++)
-  {
-    fprintf(stdout, "creating thread\n");
+  for (i = 0; i < N; i++){
     int *j = malloc(sizeof(int));
     *j = i;
     s = pthread_create(&thread[i], NULL, &init_rand, j);
-    fprintf(stdout, "thread created \n");
-    if (s != 0)
-    {
+    if (s != 0){
       fprintf(stderr, "Error, exiting \n");
       exit(1);
     }
   }
-
-  for (i = 0; i < N; i++)
-  {
+  for (i = 0; i < N; i++){
     s = pthread_join(thread[i], NULL);
-    if (s != 0)
-    {
+    if (s != 0){
       fprintf(stderr, "Error, exiting \n");
       exit(1);
     }
   }
   pthread_mutex_destroy(check);
-  for (i = 0; i < N; i++)
-  {
+  for (i = 0; i < N; i++){
     if(i != (N-1))
       fprintf(stdout, "%d, ", result[i]);
     else
@@ -60,8 +46,7 @@ int main(int argc, char *argv[])
   }
 }
 
-void *init_rand(void *args)
-{
+void *init_rand(void *args){
   int r = rand() % 1000000;
   usleep(r);
   pthread_mutex_lock(check);
