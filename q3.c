f97376dee33c2abf6c74c4c5c87f686992f30184
Stephen Carter <stephen.carter@mail.mcgill.ca>
2015-09-19 12:53:45 -0400

    Final Update

--- q3.c
+++ q3.c
@@ -10,6 +10,12 @@ int num = 0;
 pthread_mutex_t *check;
 
 int main(int argc, char *argv[]){
+
+  if argc < 2{
+    fprintf(stderr, "argc = %d; exiting\n", argc);
+    exit(1);
+  }
+
   srand(time(NULL));
 
   int N = atoi(argv[1]);
