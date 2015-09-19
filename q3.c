76f2653497d2a9d7de28435d781349d4da236e3e
Stephen Carter <stephen.carter@mail.mcgill.ca>
2015-09-19 13:03:05 -0400

    Final Update and Comments

--- q3.c
+++ q3.c
@@ -3,30 +3,29 @@
 #include <stdlib.h>
 #include <time.h>
 
-void *init_rand(void *args);
+void *init_rand(void *args);  //def functions
 
-int *result;
+int *result;                  //def required global variables
 int num = 0;
 pthread_mutex_t *check;
 
 int main(int argc, char *argv[]){
-
-  if (argc < 2){
+  if (argc < 2){              //check for cmd line entry error and exit
     fprintf(stderr, "argc = %d; exiting\n", argc);
     exit(1);
   }
 
-  srand(time(NULL));
+  srand(time(NULL));          //seed random variables and def local variables
   int N = atoi(argv[1]);
   int s;
   pthread_t thread[N];
 
-  result = malloc(sizeof(int)*N);
+  result = malloc(sizeof(int)*N);           //allocate required memory
   check = malloc(sizeof(pthread_mutex_t));
   pthread_mutex_init(check, NULL);
 
   int i;
-  for (i = 0; i < N; i++){
+  for (i = 0; i < N; i++){              //create N pthreads and check for errors
     int *j = malloc(sizeof(int));
     *j = i;
     s = pthread_create(&thread[i], NULL, &init_rand, j);
@@ -34,30 +33,32 @@ int main(int argc, char *argv[]){
       fprintf(stderr, "Error, exiting \n");
       exit(1);
     }
+    free(j);
   }
-  for (i = 0; i < N; i++){
+  for (i = 0; i < N; i++){            //join N pthreads and check for erros
     s = pthread_join(thread[i], NULL);
     if (s != 0){
       fprintf(stderr, "Error, exiting \n");
       exit(1);
     }
   }
-  pthread_mutex_destroy(check);
-  for (i = 0; i < N; i++){
+  for (i = 0; i < N; i++){            //print results in order of completion
     if(i != (N-1))
       fprintf(stdout, "%d, ", result[i]);
     else
       fprintf(stdout, "%d\n", result[i]);
   }
+  pthread_mutex_destroy(check);       //free up allocated memory
+  free(result);
 }
 
-void *init_rand(void *args){
+void *init_rand(void *args){      //function used by threads
   int r = rand() % 1000000;
   usleep(r);
-  pthread_mutex_lock(check);
+  pthread_mutex_lock(check);      //lock mutex when changing shared memory
   result[num] = *((int*)args);
   free(args);
   num++;
-  pthread_mutex_unlock(check);
+  pthread_mutex_unlock(check);    //unlock mutex for next thread and return
   return NULL;
 }
