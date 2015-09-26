f9d11e27e4460057c1859a3e1762aba39e7d629d
Stephen Carter <stephen.carter@mail.mcgill.ca>
2015-09-26 13:43:38 -0400

    b_pthread updated

--- Assignment1/binarize_pthreads.c
+++ Assignment1/binarize_pthreads.c
@@ -16,7 +16,7 @@ unsigned width = 0, height = 0;
 void *worker_thread(void *arg) {
   int *pos_val = (int*)arg;
   int value;
-  fprintf(stdout, "first height = %d\nend height = %d\nwidth = %d\nheight = %d\n", 
+  fprintf(stdout, "first height = %d\nend height = %d\nwidth = %d\nheight = %d\n",
                   pos_val[0], pos_val[1], width, height);
   for (int i = pos_val[0]; i < pos_val[1]; i++) {
     for (int j = 0; j < width; j++) {
@@ -57,7 +57,7 @@ void binarize(char* input_filename, char* output_filename, int thread_count)
   pthread_t threads[thread_count];
   for(int i = 0; i < thread_count; i++){
     pos[0] = i*height_piece;
-    pos[1] = MIN((i+1)*height_piece, height);
+    pos[1] = MIN((i+1)*height_piece+1, height);
 
     int *j = malloc(2*sizeof(int));
     memcpy(j, pos, 2*sizeof(int));
