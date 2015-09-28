3a3855b0df28d40d243fc6bda6ef6dbea410c0c3
Stephen Carter <stephen.carter@mail.mcgill.ca>
2015-09-28 09:58:49 -0400

    sobel sequential

--- Assignment1/sobel_pthreads.c
+++ Assignment1/sobel_pthreads.c
@@ -19,7 +19,7 @@ void *worker_thread(void *arg) {
   for (int i = pos_val[0]; i < pos_val[1]; i++) {
     for (int j = 1; j < width-1; j++) {
 
-      if(0 < i < height-1) {
+      if(1 < i < height-1) {
         value = (abs((image[4*width*(i-1) + 4*(j-1)] + 2*image[4*width*(i-1) + 4*j]
                   + image[4*width*(i-1) + 4*(j+1)]) - (image[4*width*(i+1) + 4*(j-1)]
                   + 2*image[4*width*(i+1) + 4*j] + image[4*width*(i+1) + 4*(j+1)]))
